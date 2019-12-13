70673321794a42d438dfc237855c947704da5868
winther32 <52148539+winther32@users.noreply.github.com>
2019-12-13 14:52:10 -0500

    Add files via upload

---
+++ eww.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+int main(){
+    printf("How many fibonacci numbers do you want to see?\n");
+    int x;
+    scanf("%d", &x);
+    while (x < 1){
+        printf("Enter a positive integer: ");
+        scanf("%d", &x);
+    }
+    int last = 1;
+    int lastlast = 0;
+
+    printf("1 ");
+    for (int i = 1; i < x; i++){
+        int current = last + lastlast;
+        printf("%d ", current);
+        lastlast = last;
+        last = current;
+    }
+
+    printf("\nDone\n");
+    
+}
