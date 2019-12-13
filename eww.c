7932380f5d77cf6e5a3fb73773590d6a4ab0ef53
Mac Lyle <maclyle@Macs-MacBook-Pro.local>
2019-12-13 15:23:48 -0500

    added defines

--- eww.c
+++ eww.c
@@ -1,20 +1,28 @@
 #include <stdio.h> 
 
-int main(){
-    printf("How many fibonacci numbers do you want to see?\n");
-    int x;
-    scanf("%d", &x);
-    while (x < 1){
-        printf("Enter a positive integer: ");
-        scanf("%d", &x);
-    }
-    int last = 1;
-    int lastlast = 0;
+#define a =
+#define aa int
+#define aaa scanf
+#define aaaa printf
+#define aAa main
+#define AaA '"'
 
-    printf("1 ");
-    for (int i = 1; i < x; i++){
-        int current = last + lastlast;
-        printf("%d ", current);
+
+aa aAa(){
+    aaaa("How many fibonacci numbers do you want to see?\n" );
+    aa x;
+    aaa("%d", &x);
+    while (x < 1){
+        aaaa("Enter a positive integer: ");
+        aaa("%d ", &x);
+    }
+    aa last = 1;
+    aa lastlast = 0;
+
+    aaaa("1 ");
+    for (aa i = 1; i < x; i++){
+        aa current = last + lastlast;
+        aaaa("%d ", current);
         lastlast = last;
         last = current;
     }
