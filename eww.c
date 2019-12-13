e301b64498bf4b91926762ab752a026c20a47899
Mac Lyle <maclyle@Macs-MacBook-Pro.local>
2019-12-13 18:57:17 -0500

    added spacing

--- eww.c
+++ eww.c
@@ -1,31 +1,29 @@
 #include <stdio.h> 
-
-#define AaaA }
-#define AAAa void
-#define aAAA (
-#define aaAa printf
-#define aaaA for
-#define AAaA <
-#define aAaA )
-#define aaaa {
-#define aAAa scanf
-#define aAaa int
-#define Aaaa >
-#define AAaa =
-#define AaAA ;
-#define AaAa -
-#define aaAA main
-
-AAAa AaA  aAAA aAaa  aA, aAaa Aa, aAaa AA aAaA AaAA aAaa aaAA aAAA
-aAaA  aaaa aaAa aAAA "%c%c%c%c%c%c:\n",76,0x65, 0x6E, 103,0x74,104
-aAaA AaAA aAaa aaaAa AaAA                aAaa A  AAaa 1  AaAA aAaa
-aAaAaa  AAaa 0 AaAA aAAa                  aAAA "%d" , & aaaAa aAaA
-AaAA  aAaa  aaAaa  AaAA        aAaa        aAaaa  AaAA  aAaa AaAaA
-AaAA aaAaa AAaa  aAaaa         AAaa         AaAaA AAaa  aaaAa AaAA
-aAAA   aAaaa  AAaA  1          aAaA          ?aaAA aAAA aAaA : AaA
-aAAA A,aAaAaa, aaaAa                          aAaA AaAA  AaaA AAAa
-AaA  aAAA  aAaa aA,          aAaa Aa,          aAaa  AA  aAaA aaaa
-aaAa aAAA "1 "aAaA          AaAA  aaaA          aAAA aAaa  A  AAaa
-1 AaAA A AAaA  AA          AaAA  A AAaa          A + 1  aAaA  aaaa
-aAaa  aaA AAaa aA + Aa AaAA aaAa aAAA "%d ",aaA aAaA  AaAA Aa AAaa
-aA  AaAA  aA  AAaa aaA  AaAA  AaaA aaAa aAAA "\n" aAaA  AaAA  AaaA
+#define AaaA                                                                          }
+#define AAAa                                                                          void  
+#define aAAA                                                                          (
+#define aaAa                                                                          printf
+#define aaaA                                                                          for
+#define AAaA                                                                          <
+#define aAaA                                                                          )
+#define aaaa                                                                          {
+#define aAAa                                                                          scanf
+#define aAaa                                                                          int
+#define Aaaa                                                                          >
+#define AAaa                                                                          =
+#define AaAA                                                                          ;     
+#define AaAa                                                                          -
+#define aaAA                                                                          main
+                AAAa AaA  aAAA aAaa  aA, aAaa Aa, aAaa AA aAaA AaAA aAaa aaAA aAAA
+                aAaA  aaaa aaAa aAAA "%c%c%c%c%c%c:\n",76,0x65, 0x6E, 103,0x74,104
+                aAaA AaAA aAaa aaaAa AaAA                aAaa A  AAaa 1  AaAA aAaa
+                aAaAaa  AAaa 0 AaAA aAAa                  aAAA "%d" , & aaaAa aAaA
+                AaAA  aAaa  aaAaa  AaAA        aAaa        aAaaa  AaAA  aAaa AaAaA
+                AaAA aaAaa AAaa  aAaaa         AAaa         AaAaA AAaa  aaaAa AaAA
+                aAAA   aAaaa  AAaA  1          aAaA          ?aaAA aAAA aAaA : AaA
+                aAAA A,aAaAaa, aaaAa                          aAaA AaAA  AaaA AAAa
+                AaA  aAAA  aAaa aA,          aAaa Aa,          aAaa  AA  aAaA aaaa
+                aaAa aAAA "1 "aAaA          AaAA  aaaA          aAAA aAaa  A  AAaa
+                1 AaAA A AAaA  AA          AaAA  A AAaa          A + 1  aAaA  aaaa
+                aAaa  aaA AAaa aA + Aa AaAA aaAa aAAA "%d ",aaA aAaA  AaAA Aa AAaa
+                aA  AaAA  aA  AAaa aaA  AaAA  AaaA aaAa aAAA "\n" aAaA  AaAA  AaaA
