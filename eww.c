35891063687eeeaf6d719a89098aa30c3fbc6c90
Mac Lyle <maclyle@Macs-MacBook-Pro.local>
2019-12-13 16:35:59 -0500

    added many defines

--- eww.c
+++ eww.c
@@ -2,31 +2,40 @@
 
 #define a =
 #define aa int
+#define AAA for
 #define aaa scanf
 #define aaAa printf
 #define aAa main
+#define Aaa void
+#define aaaa {
+#define AaaA }
+#define AaAAa (
+#define aAaA )
+#define AaAaa ;
 
-void prints(int x, int y, int z){
-    aaAa("1 ");
-    for (aa i = 1; i < z; i++){
-        aa current = x + y;
-        aaAa("%d ", current);
-        y = x;
-        x = current;
-    }
-    aaAa("\nDone\n");
-}
+Aaa AaA AaAAa aa aA, aa Aa, aa AA aAaA<%
+    aaAa AaAAa"1 "aAaA AaAaa
+    AAA AaAAa aa A a 1 AaAaa A < AA AaAaa A++aAaA aaaa
+        aa aaA a aA + Aa AaAaa
+        aaAa AaAAa"%d ", aaA aAaA AaAaa
+        Aa a aA AaAaa
+        aA a aaA AaAaa
+    AaaA
+    aaAa AaAAa"\nDone\n"aAaA AaAaa
+AaaA
 
-aa aAa(){
-    aaAa("How many fibonacci numbers do you want to see?\n" );
-    aa x;
-    aa last = 1;
-    aa lastlast = 0;
-    aaa("%d", &x);
-    (x < 1) ? 
-        ( aaAa("Enter a positive integer!\n"), 
-        main() )
+aa aAa AaAAa aAaA aaaa
+    aaAa AaAAa"How many fibonacci numbers do you want to see?\n" aAaA AaAaa
+    aa aaaAa AaAaa
+    aa A a 1 AaAaa
+    aa aAaAaa a 0 AaAaa
+    aaa AaAAa"%d", &aaaAa aAaA AaAaa
+    aa aaAaa AaAaa aa aAaaa AaAaa aa AaAaA AaAaa
+    aaAaa a aAaaa a AaAaA a aaaAa AaAaa
+    AaAAa aAaaa < 1 aAaA ? 
+        AaAAa aaAa("Enter a positive integer!\n"aAaA, 
+        aAa AaAAa aAaA aAaA
         : 
-        prints(last, lastlast, x)
-        ;     
-}
+        AaA AaAAa A,aAaAaa,aaaAa aAaA
+         AaAaa     
+AaaA
